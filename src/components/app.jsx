6ec50ec758e92a458f03882c87ad6491ba496e15
Klemen <klemen.bussines12@gmail.com>
2025-07-24 14:24:06 +0200

    Build: new component for cv

---
+++ src/components/app.jsx
@@ -0,0 +1,47 @@
+import {inputsIformations} from "./input-info"
+import CreateCv from "./create-cv-file"
+import { Fragment } from "react"
+
+function CreateInputFields({input}){
+    return(
+        <section>
+            <h3>{input.sectionName}</h3>
+            {input.inputsLabels.map((label, index)=>{
+                return(
+                    <Fragment key={input.inputId[index]}>
+                        <label htmlFor={input.inputId[index]}>{label}</label>
+                        <input type={input.inputTypes[index]} id={input.inputId[index]} />
+
+                    </ Fragment> 
+                )
+            })}
+            <button>Edit</button>
+            <button type="submit">Submit</button>
+        </section>
+    )
+}
+
+
+export default function App(){
+    return(
+        <>
+            <header>
+                <h1>CV application</h1>
+            </header>
+            <main>
+                <aside>
+                    <h2>Create your own CV here</h2>
+                    <form action="">
+                        {inputsIformations.map(info=>
+                            <CreateInputFields key={info.id} input={info} />
+                        )}
+                    </form>
+                </aside>
+                <section>
+                    <h2>Your's CV</h2>
+                    <CreateCv />
+                </section>
+            </main>
+        </>
+    )
+}
---
+++ src/components/create-cv-file.jsx
@@ -0,0 +1,7 @@
+export default function CreateCv(){
+    return(
+        <section>
+            <strong>Kfds Ffdsfd</strong>
+        </section>
+    )
+}
