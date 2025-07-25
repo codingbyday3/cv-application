1a061358771dc1ce762ea474294bebc7a6ae5108
Klemen <codingbyday3@users.noreply.github.com>
2025-07-25 09:43:57 +0200

    Change: every section heading to button

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,18 @@
 import {inputsIformations} from "./input-info"
 import CreateCv from "./create-cv-file"
 import "../styles/app.css"
+import { useState } from "react"
 
 function CreateInputFields({input}){
     return(
         <form action="">
-            <div className="burger-menu">
+            <button className="burger-menu">
                 <h3>{input.sectionName}</h3>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <title>chevron-down</title>
                 <path d="M5.84,9.59L11.5,15.25L17.16,9.59L16.45,8.89L11.5,13.84L6.55,8.89L5.84,9.59Z" /></svg>
             
-            </div>
+            </button>
             {input.inputsLabels.map((label, index)=>{
                 return(
                     <div className="input-container" key={input.inputId[index]}>
@@ -32,6 +33,7 @@ function CreateInputFields({input}){
 
 
 export default function App(){
+    const [openIndex, setOpenIndex] = useState(0)
     return(
         <>
             <header>
@@ -40,8 +42,8 @@ export default function App(){
             <main>
                 <aside>
                     <h2>Create your own <span>CV</span> here</h2>
-                        {inputsIformations.map(info=>
-                            <CreateInputFields key={info.id} input={info} />
+                        {inputsIformations.map((info, index)=>
+                            <CreateInputFields key={info.id} input={info} isActive={openIndex === index}/>
                         )}
                 </aside>
                 <section className="cv-container">
