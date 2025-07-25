423e424006a09cf71c79517aa1ceff1b2f9f801c
Klemen <codingbyday3@users.noreply.github.com>
2025-07-25 15:53:41 +0200

    Build: inputs only show when you click on section

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,32 +3,45 @@ import CreateCv from "./create-cv-file"
 import "../styles/app.css"
 import { useState } from "react"
 
-function CreateInputFields({input}){
-    return(
-        <form action="">
-            <button className="burger-menu">
-                <h3>{input.sectionName}</h3>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <title>chevron-down</title>
-                <path d="M5.84,9.59L11.5,15.25L17.16,9.59L16.45,8.89L11.5,13.84L6.55,8.89L5.84,9.59Z" /></svg>
-            
-            </button>
-            {input.inputsLabels.map((label, index)=>{
-                return(
-                    <div className="input-container" key={input.inputId[index]}>
-                        <label htmlFor={input.inputId[index]}>{label}</label>
-                        <input type={input.inputTypes[index]} id={input.inputId[index]} />
-                    </div>
+function CreateInputFields({input, isActive, onShow}){
 
-                )
-            })}
-            <div className="buttons-container">
-                <button>Edit</button>
-                <button type="submit">Submit</button>
+  return (
+    <form action="">
+      <button
+        className="burger-menu"
+        onClick={(e) => {
+          e.preventDefault();
+          onShow();
+        }}
+      >
+        <h3>{input.sectionName}</h3>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <title>chevron-down</title>
+          <path d="M5.84,9.59L11.5,15.25L17.16,9.59L16.45,8.89L11.5,13.84L6.55,8.89L5.84,9.59Z" />
+        </svg>
+      </button>
+
+      {isActive && (
+        <>
+          {input.inputsLabels.map((label, index) => (
+            <div className="input-container" key={input.inputId[index]}>
+              <label htmlFor={input.inputId[index]}>{label}</label>
+              <input
+                type={input.inputTypes[index]}
+                id={input.inputId[index]}
+              />
             </div>
+          ))}
+          <div className="buttons-container">
+            <button>Edit</button>
+            <button type="submit">Submit</button>
+          </div>
+        </>
+      )}
+    </form>
+  );
 
-        </form>
-    )
+
 }
 
 
@@ -43,7 +56,7 @@ export default function App(){
                 <aside>
                     <h2>Create your own <span>CV</span> here</h2>
                         {inputsIformations.map((info, index)=>
-                            <CreateInputFields key={info.id} input={info} isActive={openIndex === index}/>
+                            <CreateInputFields key={info.id} input={info} isActive={openIndex === index} onShow={() => setOpenIndex(index)}/>
                         )}
                 </aside>
                 <section className="cv-container">
